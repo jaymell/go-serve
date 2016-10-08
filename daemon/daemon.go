ae641bbd21f8bdbc9afa5f4b4378f6af404b1a3a
James Lucas <jamesmlucas@gmail.com>
2016-10-08 07:22:23 +0000

    move data funcs out of daemon

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -147,40 +147,6 @@ func (d *Daemon) Start() {
 	http.Serve(d.Listener, nil)
 }
 
-func (d *Daemon) GetData() (interface{}, error) {
-	URL, err := url.Parse(d.Config.DataURL)
-	if err != nil {
-		return nil, fmt.Errorf("could not parse url from config: ", err)
-	}
-	switch URL.Scheme {
-	case "mongodb":
-		return getMongoData(d.Config.DataURL, d.Config.CollectionName)
-	case "http":
-		return nil, fmt.Errorf("Not implemented yet")
-	default:
-		return nil, fmt.Errorf("Unrecognized scheme")
-	}
-}
-
-func getMongoData(URL string, col string) (interface{}, error) {
-	dialInfo, err := mgo.ParseURL(URL)
-	if err != nil {
-		return nil, fmt.Errorf("unable to parse db url: ", err)
-	}
-
-	dialInfo.Timeout = time.Duration(5) * time.Second
-	session, err := mgo.DialWithInfo(dialInfo)
-
-	if err != nil {
-		return nil, fmt.Errorf("unable to connect to db: ", err)
-	}
-	c := session.DB(dialInfo.Database).C(col)
-	results := []Data{}
-	c.Find(nil).All(&results)
-	// FIXME -- don't always return nil for err, probably:
-	return results, nil
-}
-
 func SyncResponse(result interface{}) Response {
 	if _, ok := result.(error); ok {
 		return &resp{
